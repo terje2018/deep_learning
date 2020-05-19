6dc793f7c7ef44ef49688fc5b72d93457e7b9bf2
terje2018 <terjeliu2018@gmail.com>
2020-05-19 22:33:19 +0800

    idea of lecun-5

---
+++ cnn/lecun_conv_net.py
@@ -0,0 +1,38 @@
+from keras.datasets import mnist
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.utils import Sequence
+import numpy as np
+from cnn.DataSequence import MNISTSequence
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#X = X_train[1]/255
+#TX = X_test/255
+#Y = to_categorical(y_train,10)[1]
+#TY = to_categorical(y_test,10)
+
+x_train = x_train.reshape(x_train.shape[0],28,28,1)/255
+x_test = x_test.reshape(x_test.shape[0],28,28,1)/255
+y_train = to_categorical(y_train,num_classes=10)
+y_test = to_categorical(y_test,num_classes=10)
+
+model = Sequential()
+model.add(Conv2D(16,(5,5),activation='relu',kernel_initializer='glorot_normal', input_shape=(28,28,1)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32,(3,3),activation='relu',kernel_initializer='glorot_normal'))
+model.add(MaxPooling2D())
+model.add(Conv2D(64,(3,3),activation='relu',kernel_initializer='glorot_normal'))
+#model.add(MaxPooling3D(data_format='channels_last'))
+model.add(Flatten())
+model.add(Dense(576, activation='relu'))
+model.add(Dropout(0.15))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+model.fit(x_train,y_train,1, epochs=32, shuffle=True,validation_data=(x_test,y_test))
+
